359cfddad6f8e655debc53bc5a61a1d671482eb7
jimmy <jimmy>
2024-01-20 11:42:28 +0700

    Menghapus fitur yang tidak berguna

--- src/main/resources/db/migration/V5__create_feedbacks_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE feedbacks (
-  id integer AUTO_INCREMENT PRIMARY KEY,
-  reservation_id integer,
-  attendances varchar(255),
-  meeting_notes text
-);
-
-ALTER TABLE feedbacks ADD FOREIGN KEY (reservation_id) REFERENCES reservations (id);
